59c99ba8d43d1c6de3ff3a5bea06112699979d43
sheela <sheelasankanur06@gmail.com>
2026-03-13 18:11:56 +0530

    this is prompt js

---
+++ prompt1.js
@@ -0,0 +1,11 @@
+let h1=document.querySelector("h1")
+let name=prompt("enter name")
+url=("https://api.agify.io/?name="+ name)
+fetch(url)   //fetch method return promise
+.then((Response)=>{
+    console.log(Response)
+    return Response.json()
+})
+then((data)=>{
+    h1.textContent=`${data.name} is ${data.age}`
+})
