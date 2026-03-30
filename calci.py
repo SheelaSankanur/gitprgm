590d2300541fde33d7d3869b6141eab19b0988a2
sheela <sheelasankanur06@gmail.com>
2026-03-30 22:42:06 +0530

    this is calculator

---
+++ calci.py
@@ -0,0 +1,26 @@
+print("Simple calculator")
+print("Select an operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+print("5. Exit")
+while True:
+    operation = int(input("Enter your choice (1-5): "))
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if operation==1:
+        print("Result:", num1 + num2)
+    elif operation==2:
+        print("Result:", num1 - num2)
+    elif operation==3:
+        print("Result:", num1 * num2)
+    elif operation==4:
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Division by zero is not allowed.")  
+    else:
+        if operation==5:
+            print("Exiting the calculator. Goodbye!")
+            break
