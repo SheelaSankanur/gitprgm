e5da5a6f3b500caaa5cbb4fa71c2e03f6305c5a2
sheela <sheelasankanur06@gmail.com>
2026-04-02 20:18:25 +0530

    this is two pointers

---
+++ twopointers.py
@@ -0,0 +1,12 @@
+def two_sum(arr,target):
+    l,r=0,len(arr)-1
+    while l<r:
+        s=arr[l]+arr[r]
+        if s==target:
+            return [l,r] 
+        elif s<target:
+            l+=1
+        else:
+            r-=1
+    return None 
+print(two_sum([1,2,3,4,5],9)) 
