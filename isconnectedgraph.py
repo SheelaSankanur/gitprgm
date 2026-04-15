137d1cf145155feddb7478714d9cffbd8375be9e
sheela <sheelasankanur06@gmail.com>
2026-04-15 21:53:43 +0530

    this is isconnectedgraph.py

---
+++ isconnectedgraph.py
@@ -0,0 +1,17 @@
+def add_edge(graph, u, v):
+    if u not in graph:
+        graph[u] = []
+    if v not in graph:
+        graph[v] = []
+    graph[u].append(v)
+    graph[v].append(u)   # undirected
+
+graph = {}
+add_edge(graph, 0, 1)
+add_edge(graph, 1, 2)
+add_edge(graph, 0, 3)
+def is_connected(graph,u,v):
+    if u not in graph:
+        return False
+    return v in graph[u]
+print(is_connected(graph,0,1))
